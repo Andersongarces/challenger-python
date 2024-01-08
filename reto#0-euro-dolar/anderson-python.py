605c1b9e89bb67b750b2716ce30609af4bca26e9
Andersongarces <146659066+Andersongarces@users.noreply.github.com>
2024-01-07 19:23:15 -0500

    Delete reto#0-euro-dolar/anderson-python.py

--- reto#0-euro-dolar/anderson-python.py
+++
@@ -1,8 +0,0 @@
-
-# Reto #0 - Convertir de Euro a dólar
-
-euro_ingresado = float(input("Ingrese la cantidad en euros: "))
-
-cambio_dolar = euro_ingresado*1.09475
-
-print(f" {euro_ingresado} euros en dólares son {cambio_dolar}") 
